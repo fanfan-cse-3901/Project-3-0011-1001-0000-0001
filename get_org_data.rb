bfcd7a919abcd0164c1ce9fea0b17f973272fac3
Amanda <cheng.1319@osu.edu>
2020-06-12 14:55:04 -0400

    Modified org.rb to include name and reconstructured get_org_data.rb

--- get_org_data.rb
+++ get_org_data.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-# File created 06/11/2020 by Yifan Yao
+# File created 06/11/2020 by Yifan Yao: created URL
+# Edited 06/11/2020 by Amanda Cheng: Reconstructed entire get_org_data and stored every information into Org Object
 require 'mechanize'
 require './org.rb'
 
@@ -18,70 +19,73 @@ def get_org_data orgs
     page = agent.get org_url
 
     # get values from form via XPath
+    name = page.search('//div/h4')
     table = page.search('//form/div/table/tr')
-    # attributes = page.search('//form/div/table/tr/td')
+    # store into name object
+    i.name = name.text
 
-    #puts table[]
-    # sanatize spaces then output
-    #  table.each do |j|
-    #    puts j.text.split(':') # .join(' ')
-    # #   # TODO: put values into object
-    #  end
     count = 0
     while count < table.length()
       combo = table[count].text.split(':')
-      if combo[0].include? "Campus"
+      if combo[0].include? 'Campus'
         i.campus = combo[1].strip
-        puts i.campus
-      elsif combo[0].include? "Status"
+        # puts i.campus
+      elsif combo[0].include? 'Status'
         i.status = combo[1].strip
-      elsif combo[0].include? "Purpose"
+      elsif combo[0].include? 'Purpose'
         i.purpose = combo[1].strip
-      elsif combo[0].include? "Primary Leader"
+      elsif combo[0].include? 'Primary Leader'
         i.p_leader = combo[1].strip
-        puts i.p_leader
-      elsif combo[0].include? "Treas"
+      elsif combo[0].include? 'Treas'
         i.t_leader = combo[1].strip
-      elsif combo[0].include? "Advisor"
+      elsif combo[0].include? 'Advisor'
         i.advisor = combo[1].strip
-      elsif combo[0].include? "Email"
+      elsif combo[0].include? 'Email'
         i.email = combo[1].strip
-        puts i.email
-      elsif combo[0].include? "Website"
-        i.website = combo[1].strip
-      elsif combo[0].include? "Facebook"
-        i.website = combo[1].strip
+      elsif combo[0].include? 'Website'
+        # Since we split by :, links often have : so we repatch it here
+        link_count = 2
+        link = combo[1].strip
+        while link_count < combo.length()
+          link += ':' + combo[link_count].strip
+          link_count += 1
+        end
+        i.website = link
+         # puts i.website
+      elsif combo[0].include? 'Facebook'
+        # Since we split by :, links often have : so we repatch it here
+        link_count = 2
+        link = combo[1].strip
+        while link_count < combo.length()
+          link += ':' + combo[link_count].strip
+          link_count += 1
+        end
+        i.facebook = link
+         # puts i.facebook
+      elsif combo[0].include? 'Primary Type'
+        i.p_type = combo[1].strip
+      elsif combo[0].include? 'Secondary Type'
+        i.s_type = combo[1].strip
+      elsif combo[0].include? 'Make Up'
+        i.make_up = combo[1].strip
+      elsif combo[0].include? 'Constitution'
+        i.constitution = combo[1].strip
+      elsif combo[0].include? 'Meeting Time'
+        i.time_place = combo[1].strip
+      elsif combo[0].include? 'Office Location'
+        i.office_location = combo[1].strip
+      elsif combo[0].include? 'Membership Type'
+        i.membership_type = combo[1].strip
+      elsif combo[0].include? 'Membership Contact'
+        i.membership_contact = combo[1].strip
+      elsif combo[0].include? 'Time of Year'
+        i.new_membership_time = combo[1].strip
+      elsif combo[0].include? 'How'
+        i.how_to_apply = combo[1].strip
+      elsif combo[0].include? 'Charge'
+        i.charge_dues = combo[1].strip
       end
       count += 1
     end
-    # i.campus = table[0].text.split.join(' ')
-    # i.status = table[1].text.split.join(' ')
-    # i.purpose = table[2].text.split.join(' ')
-    # i.p_leader = table[3].text.split.join(' ')
-    # i.s_leader = table[4].text.split.join(' ')
-    # i.t_leader = table[5].text.split.join(' ')
-    # i.advisor = table[6].text.split.join(' ')
-    # i.email = table[7].text.split.join(' ')
-    # i.website = table[8].text.split.join(' ')
-    # i.p_type = table[9].text.split.join(' ')
-    # i.s_type = table[10].text.split.join(' ')
-    # if table[11].text != nil
-    #   i.make_up = table[11].text.split.join(' ')
-    #   #puts i.make_up
-    # end
-    #
-    # i.constitution = table[12].text.split.join(' ')
-    # i.time_place = table[13].text.split.join(' ')
-    # i.office_location = table[14].text.split.join(' ')
-    # i.membership_type = table[15].text.split.join(' ')
-    # i.membership_contact = table[16].text.split.join(' ')
-    # i.new_membership_time = table[17].text.split.join(' ')
-    # i.how_to_apply = table[18].text.split.join(' ')
-    # if table[19].text != nil
-    #   i.charge_dues = table[19].text.split.join(' ')
-    #   #puts i.charge_dues
-    # end
-
-    #puts i.make_up
   end
 end
--- org.rb
+++ org.rb
@@ -4,9 +4,10 @@
 # Class Org is a place to store values scraped from web pages.
 
 # Created 06/10/2020 by Yifan Yao
+# Edited 06/11/2020 by Amanda Cheng: Defined the structure of Org object with its many attributes
 class Org
   attr_reader :id
-  attr_accessor :pic_url, :campus, :status, :purpose, :p_leader,
+  attr_accessor :name, :pic_url, :campus, :status, :purpose, :p_leader,
                 :s_leader, :t_leader, :advisor, :email, :website, :facebook, :p_type,
                 :s_type, :make_up, :constitution, :time_place, :office_location,
                 :membership_type, :membership_contact, :new_membership_time, :how_to_apply, :charge_dues
@@ -16,26 +17,27 @@ class Org
   # Initialize class Org with the unique organization id
   def initialize(org_id)
     @id = org_id
-    # @campus = 'N/A'
-    # @status = 'N/A'
-    # @purpose = 'N/A'  # Purpose Statement
-    # @p_leader = 'N/A'  # Primary Leader
-    # @s_leader = 'N/A'  # Secondary Leader
-    # @t_leader = 'N/A'  # Treasury Leader
-    # @advisor = 'N/A'
-    # @email = 'N/A'
-    # @website = 'N/A'
-    # @p_type = 'N/A'  # Primary Type
-    # @s_type = 'N/A'  # Secondary Type
-    # @make_up = 'N/A' # Primary Make Up (undergraduate vs graduate)
-    # @constitution = 'N/A'
-    # @time_place = 'N/A'
-    # @office_location = 'N/A'
-    # @membership_type = 'N/A'
-    # @membership_contact = 'N/A'
-    # @new_membership_time = 'N/A'
-    # @how_to_apply = 'N/A'
-    # @charge_dues = 'N/A'
+    @campus = 'N/A'
+    @status = 'N/A'
+    @purpose = 'N/A'  # Purpose Statement
+    @p_leader = 'N/A'  # Primary Leader
+    @s_leader = 'N/A'  # Secondary Leader
+    @t_leader = 'N/A'  # Treasury Leader
+    @advisor = 'N/A'
+    @email = 'N/A'
+    @website = 'N/A'
+    @facebook = 'N/A'
+    @p_type = 'N/A'  # Primary Type
+    @s_type = 'N/A'  # Secondary Type
+    @make_up = 'N/A' # Primary Make Up (undergraduate vs graduate)
+    @constitution = 'N/A'
+    @time_place = 'N/A'
+    @office_location = 'N/A'
+    @membership_type = 'N/A'
+    @membership_contact = 'N/A'
+    @new_membership_time = 'N/A'
+    @how_to_apply = 'N/A'
+    @charge_dues = 'N/A'
   end
 
   public :initialize
