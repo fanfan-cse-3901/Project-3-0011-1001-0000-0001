55afaa09eb7318a856e15b1a1454929143caae95
gobletFire <garge.2@osu.edu>
2020-06-12 18:55:16 -0400

    Updated the main method. (Also, need error fixing in some test cases.)

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # File created 06/10/2020 by Troy Stein
-
+# Edited on 06/12/2020 by Prachiti Garge: Updated method call
 =begin
 Main file.
 =end
@@ -15,7 +15,7 @@ require './output.rb'
 
 # MAIN
 puts 'Welcome to The Ohio State University Clubs page!'
-url = basic_search # Call basic_search to trim list of organizations
+url = Searching.basic_search # Call basic_search to trim list of organizations
 orgs = [] # orgs - array to store Org objects
 get_org_list url, orgs # stores objects into array orgs
 get_org_data orgs # populates Org objects with respective data
