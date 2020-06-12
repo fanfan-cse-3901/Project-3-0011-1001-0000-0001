8658497466b350d47a0259c288b48ccf0519f9ea
gobletFire <garge.2@osu.edu>
2020-06-12 18:47:07 -0400

    Completed implementation and test plan for basic_search.

--- basic_search.rb
+++ basic_search.rb
@@ -1,1 +1,188 @@
+# frozen_string_literal: true
+
+# This module performs the searches to trim the list of student organizations.
+# Has method basic_search.
+# Created on 06/10/2020 by Prachiti Garge
+# Edited on 06/12/2020 by Prachiti Garge: Added mechanize
+# DO NOT FIX ALL OFFENCES ON THIS PAGE DIRECTLY
+
+require 'mechanize'
+require 'cgi'
+
+# Module Searching has all the methods that get user requirements for showing organizations
+module Searching
+  # @return url to the first page of searches
+  # Created on 06/10/2020 by Prachiti Garge
+  # Edited on 06/12/2020 by Prachiti Garge: Used mechanize and regex
+  def self.basic_search
+    agent = Mechanize.new
+    page = agent.get 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org/?v=list'
+    val = 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org/?v=list' + Searching.get_campus(page) +
+          Searching.get_directory + Searching.get_search
+    puts 'Do you want to perform advanced search? (y/n): '
+    input = gets.chomp
+    while !(/^[ynYN]$/.match? input)
+      puts 'Invalid input! Do you want to perform advanced search? (y/n): '
+      input = gets.chomp
+    end
+    val += Searching.get_category(page) + Searching.get_text_type(page) + Searching.get_reg_win(page) + Searching.show_inactive_org if /[yY]/.match? input
+    val
+  end
+
+  # Asks user for campus and returns addition to url
+  # Created on 06/10/2020 by Prachiti Garge
+  # Edited on 06/12/2020 by Prachiti Garge: Used mechanize and regex
+  def self.get_campus(page)
+    ret = ''
+    list = page.search('//div/select/option')
+    campuses = []
+    list.each do |i|
+      campuses.append(i.text.split.join(' '))
+    end
+    (0...campuses.length).each { |i|
+      puts "#{i + 1}: #{campuses[i]}"
+    }
+    puts "Enter a number between 1 and #{campuses.length} for a campus: "
+    input = gets.chomp
+    while (!(/[1-9][0-9]*/.match? input) && input.to_i >= campuses.length)
+      puts "Invalid input! Enter a number between 1 and #{campuses.length}: "
+      input = gets.chomp
+    end
+    ret = "&c=#{campuses[input.to_i - 1]}"
+    ret
+  end
+
+  # Asks user for directory and returns addition to url
+  # Created on 06/10/2020 by Prachiti Garge
+  # Edited on 06/12/2020 by Prachiti Garge: Used mechanize and regex
+  def self.get_directory
+    puts 'Enter an alphabet (1 for numbers, 0 for all) to search directory: '
+    input = gets.chomp
+    while !(/[A-Za-z01]/.match(input))
+      puts 'Invalid input! Enter a letter (1 for numbers, 0 for all) to search directory: '
+      input = gets.chomp
+    end
+    ret = if input == '0'
+            '&l=ALL'
+          elsif input == '1'
+            '&l=1'
+          else
+            "&l=#{input.capitalize}"
+          end
+    ret
+  end
+
+  # Asks user for search term and returns addition to url
+  # Created on 06/10/2020 by Prachiti Garge
+  # Edited on 06/12/2020 by Prachiti Garge: Used mechanize and regex
+  def self.get_search
+    puts 'Type a search term (press enter to skip): '
+    input = gets
+    ret = ''
+    ret = "&s=#{CGI.escape(input.chomp)}" if input != '\n'
+    ret
+  end
+
+  # Asks user for categories and returns addition to url
+  # Can have multiple categories
+  # Created on 06/10/2020 by Prachiti Garge
+  # Edited on 06/12/2020 by Prachiti Garge: Used mechanize and regex
+  def self.get_category(page)
+    ret = ''
+    list = page.search('//div/span[@id="ctl00_ContentBody_pageFormControl_cbl_org_make_up"]/label')
+    categories = []
+    list.each do |i|
+      categories.append(i.text.split.join(' '))
+    end
+    (0...categories.length).each { |i|
+      puts "#{i + 1}: #{categories[i]}"
+    }
+    input = []
+    puts "Enter a number between 1 and #{categories.length} per line for a category (0 for all, press enter to end category selection): "
+    input_val = gets.chomp
+    while input_val != '-1'
+      while !(/[1-9][0-9]*/.match? input_val) && input_val.to_i >= categories.length
+        puts "Invalid input! Enter a number between 1 and #{categories.length}: "
+        input_val = gets.chomp
+        break if input_val == '0'
+      end
+      break if input_val == '0'
+
+      input.append(input_val)
+    end
+    unless input.length.zero?
+      input.to_i.sort!
+      unless input.include? '0'
+        ret = "&m=#{CGI.escape(categories[input[0].to_i - 1])}"
+        (1...input.length).each { |i|
+          ret += "+#{CGI.escape(categories[input[i].to_i - 1])}"
+        }
+      end
+    end
+    ret
+  end
+
+  # Asks user for text type and returns addition to url
+  # Created on 06/10/2020 by Prachiti Garge
+  # Edited on 06/12/2020 by Prachiti Garge: Used mechanize and regex
+  def self.get_text_type(page)
+    ret = ''
+    list = page.search('//div/span[@id="ctl00_ContentBody_pageFormControl_rbl_search_type"]/label')
+    types = []
+    list.each do |i|
+      types.append(i.text.split.join(' '))
+    end
+    (0...types.length).each { |i|
+      puts "#{i + 1}: #{types[i]}"
+    }
+    puts "Enter a number between 1 and #{types.length} for searching text type: "
+    input = gets.chomp
+    while !(/[1-9][0-9]*/.match? input) && input.to_i >= types.length
+      puts "Invalid input! Enter a number between 1 and #{types.length}: "
+      input = gets.chomp
+    end
+    ret = "&t=#{types[input.to_i - 1].downcase}"
+    ret
+  end
+
+  # Asks user for registration window and returns addition to url
+  # Created on 06/10/2020 by Prachiti Garge
+  # Edited on 06/12/2020 by Prachiti Garge: Used mechanize and regex
+  def self.get_reg_win(page)
+    ret = ''
+    list = page.search('//div/span[@id="ctl00_ContentBody_pageFormControl_rbl_ott"]/label')
+    win = []
+    list.each { |i| win.append i.text.split.join '' }
+    (0...win.length).each { |i|
+      puts "#{i + 1}: #{win[i]}"
+    }
+    puts "Enter a number between 1 and #{win.length} for a registration window: "
+    input = gets.chomp
+    while (!(/[1-9][0-9]*/.match? input) && input.to_i >= win.length)
+      puts 'Invalid input! Enter a number between 1 and 7: '
+      input = gets.chomp
+    end
+    ret = "&ot=#{input.to_i - 1}"
+    ret
+  end
+
+  # Asks user if they want to see inactive clubs and returns addition to url
+  # Created on 06/10/2020 by Prachiti Garge
+  # Edited on 06/12/2020 by Prachiti Garge: Used mechanize and regex
+  def self.show_inactive_org
+    ret = ''
+    puts 'Do you want to see inactive organizations? (y/n): '
+    input = gets.chomp
+    while !(/^[ynYN]$/.match? input)
+      puts 'Invalid input! Do you want to see inactive organizations? (y/n): '
+      input = gets.chomp
+    end
+    ret = if /[yY]/.match? input
+            '&a=0'
+          else
+            '&a=1'
+          end
+    ret
+  end
+end
 
--- spec/basic_search_spec.rb
+++ spec/basic_search_spec.rb
@@ -14,199 +14,199 @@ RSpec.describe Searching do
   page = Mechanize.new.get 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org/?v=list'
   describe 'get_campus' do
     it 'gives right extension for all campuses' do
-      input = StringIO.new('1')
-      $stdin = input
-      expect (get_campus page).to eq('&c=all')
+      in1 = StringIO.new('1\n')
+      $stdin = in1
+      expect(Searching.get_campus(page)).to(eq('&c=All'))
       $stdin = STDIN
     end
 
     it 'gives right extension for Lima' do
-      input = StringIO.new('3')
-      $stdin = input
-      expect (get_campus page).to eq('&c=Lima')
+      in2 = StringIO.new('3\n')
+      $stdin = in2
+      expect(Searching.get_campus (page)).to(eq('&c=Lima'))
       $stdin = STDIN
     end
 
     it 'gives right extension for Wooster' do
-      input = StringIO.new('7')
-      $stdin = input
-      expect (get_campus page).to eq('&c=Wooster')
+      in3 = StringIO.new('7')
+      $stdin = in3
+      expect(Searching.get_campus (page)).to(eq('&c=Wooster'))
       $stdin = STDIN
     end
 
     it 'gives right extension for Mansfield' do
-      input = StringIO.new('8')
-      input.puts '4'
-      $stdin = input
-      expect (get_campus page).to eq('&c=Mansfield')
+      in4 = StringIO.open('8')
+      in4.puts '4'
+      $stdin = in4
+      expect(Searching.get_campus (page)).to(eq('&c=Mansfield'))
       $stdin = STDIN
     end
   end
 
   describe 'get_directory' do
     it 'gives right extension for numbers directory' do
-      input = StringIO.new('1')
-      $stdin = input
-      expect (get_directory page).to eq('&l=1')
+      in5 = StringIO.new('1')
+      $stdin = in5
+      expect(Searching.get_directory).to(eq('&l=1'))
       $stdin = STDIN
     end
 
     it 'gives right extension for S directory' do
-      input = StringIO.new('S')
-      $stdin = input
-      expect (get_directory page).to eq('&l=S')
+      in6 = StringIO.new('S')
+      $stdin = in6
+      expect(Searching.get_directory).to(eq('&l=S'))
       $stdin = STDIN
     end
 
     it 'gives right extension for all directory' do
-      input = StringIO.new('0')
-      $stdin = input
-      expect (get_directory page).to eq('&l=ALL')
+      in7 = StringIO.new('0')
+      $stdin = in7
+      expect(Searching.get_directory).to(eq('&l=ALL'))
       $stdin = STDIN
     end
 
     it 'gives right extension for F directory' do
-      input = StringIO.new('abc')
-      input.puts 'f'
-      $stdin = input
-      expect (get_directory page).to eq('&l=F')
+      in8 = StringIO.open('abc')
+      in8.puts 'f'
+      $stdin = in8
+      expect(Searching.get_directory).to(eq('&l=F'))
       $stdin = STDIN
     end
   end
 
   describe 'get_search' do
     it 'gives right extension for search of ab74' do
-      input = StringIO.new('ab74')
-      $stdin = input
-      expect (get_search page).to eq('&s=ab74')
+      in9 = StringIO.new('ab74')
+      $stdin = in9
+      expect(Searching.get_search).to(eq('&s=ab74'))
       $stdin = STDIN
     end
 
     it 'gives right extension for search of Bull\'s eye' do
-      input = StringIO.new('Bull\'s eye')
-      $stdin = input
-      expect (get_search page).to eq('&s=Bull%27s+eye')
+      in10 = StringIO.new('Bull\'s eye')
+      $stdin = in10
+      expect(Searching.get_search).to(eq('&s=Bull%27s+eye'))
       $stdin = STDIN
     end
 
     it 'gives right extension for search of .\';l\'.;\'4r\\' do
-      input = StringIO.new('.\';l\'.;\'4r\\')
-      $stdin = input
-      expect (get_search page).to eq('&s=.%27%3bl%27.%3b%274r%5c')
+      in11 = StringIO.new('.\';l\'.;\'4r\\')
+      $stdin = in11
+      expect(Searching.get_search).to(eq('&s=.%27%3Bl%27.%3B%274r%5C'))
       $stdin = STDIN
     end
 
     it 'gives right extension for search of \n' do
-      input = StringIO.new('\n')
-      $stdin = input
-      expect (get_search page).to eq('')
+      in12 = StringIO.new('\n')
+      $stdin = in12
+      expect(Searching.get_search).to(eq(''))
       $stdin = STDIN
     end
   end
 
   describe 'get_category' do
     it 'gives right extension for all categories' do
-      input = StringIO.new('0')
-      $stdin = input
-      expect (get_category page).to eq('')
+      in13 = StringIO.new('0')
+      $stdin = in13
+      expect(Searching.get_category (page)).to(eq(''))
       $stdin = STDIN
     end
 
     it 'gives right extension for Community Service/Service Learning category' do
-      input = StringIO.new('3')
-      input.puts '\n'
-      $stdin = input
-      expect (get_category page).to eq('&m=Community+Service%2fService+Learning')
+      in14 = StringIO.open('3')
+      in14.puts '\n'
+      $stdin = in14
+      expect(Searching.get_category (page)).to(eq('&m=Community+Service%2fService+Learning'))
       $stdin = STDIN
     end
 
     it 'gives right extension for Ethnic/Cultural,Governance Organizations, Special Interest categories' do
-      input = StringIO.new('6')
-      input.puts '11'
-      input.puts 'abe0'
-      input.puts '5'
-      input.puts '\n'
-      $stdin = input
-      expect (get_category page).to eq('&m=Ethnic%2fCultural%3bGovernance+Organizations%3bSpecial+Interest')
+      in15 = StringIO.open('6')
+      in15.puts '11'
+      in15.puts 'abe0'
+      in15.puts '5'
+      in15.puts '\n'
+      $stdin = in15
+      expect(Searching.get_category (page)).to(eq('&m=Ethnic%2fCultural%3bGovernance+Organizations%3bSpecial+Interest'))
       $stdin = STDIN
     end
   end
 
   describe 'get_text_type' do
     it 'gives right extension for search type all' do
-      input = StringIO.new('1')
-      $stdin = input
-      expect (get_text_type page).to eq('&t=all')
+      in16 = StringIO.new('1')
+      $stdin = in16
+      expect(Searching.get_text_type (page)).to(eq('&t=all'))
       $stdin = STDIN
     end
 
     it 'gives right extension for search type purpose' do
-      input = StringIO.new('3')
-      $stdin = input
-      expect (get_text_type page).to eq('&t=purpose')
+      in17 = StringIO.new('3')
+      $stdin = in17
+      expect(Searching.get_text_type (page)).to(eq('&t=purpose'))
       $stdin = STDIN
     end
 
     it 'gives right extension for search type name' do
-      input = StringIO.new('How are you?')
-      input.puts '2'
-      $stdin = input
-      expect (get_text_type page).to eq('&t=name')
+      in18 = StringIO.new('How are you?')
+      in18.puts '2'
+      $stdin = in18
+      expect(Searching.get_text_type (page)).to(eq('&t=name'))
       $stdin = STDIN
     end
   end
 
   describe 'get_reg_win' do
     it 'gives right extension for all reg windows' do
-      input = StringIO.new('1')
-      $stdin = input
-      expect (get_reg_win page).to eq('&ot=0')
+      in19 = StringIO.new('1')
+      $stdin = in19
+      expect(Searching.get_reg_win (page)).to(eq('&ot=0'))
       $stdin = STDIN
     end
 
     it 'gives right extension for reg window in Spring' do
-      input = StringIO.new('2')
-      $stdin = input
-      expect (get_reg_win page).to eq('&ot=1')
+      in20 = StringIO.new('2')
+      $stdin = in20
+      expect(Searching.get_reg_win (page)).to(eq('&ot=1'))
       $stdin = STDIN
     end
 
     it 'gives right extension for reg window in Autumn' do
-      input = StringIO.new('3')
-      $stdin = input
-      expect (get_reg_win page).to eq('&ot=2')
+      in21 = StringIO.new('3')
+      $stdin = in21
+      expect(Searching.get_reg_win (page)).to(eq('&ot=2'))
       $stdin = STDIN
     end
 
     it 'gives right extension for reg window in Autumn' do
-      input = StringIO.new('Bamboozled')
-      input.puts '3'
-      $stdin = input
-      expect (get_reg_win page).to eq('&ot=2')
+      in22 = StringIO.open('Bamboozled')
+      in22.puts '3'
+      $stdin = in22
+      expect(Searching.get_reg_win (page)).to(eq('&ot=2'))
       $stdin = STDIN
     end
   end
 
   describe 'show_inactive_org' do
     it 'gives right extension for showing inactive organizations' do
-      input = StringIO.new('y')
-      $stdin = input
-      expect (show_inactive_org page).to eq('&a=0')
+      in23 = StringIO.new('y')
+      $stdin = in23
+      expect(Searching.show_inactive_org).to(eq('&a=0'))
       $stdin = STDIN
     end
 
     it 'gives right extension for showing only active organizations' do
-      input = StringIO.new('N')
-      $stdin = input
-      expect (show_inactive_org page).to eq('&a=1')
+      in24 = StringIO.new('N')
+      $stdin = in24
+      expect(Searching.show_inactive_org).to(eq('&a=1'))
       $stdin = STDIN
     end
 
     it 'gives right extension for showing only active organizations' do
-      input = StringIO.new('Kaboom98')
-      input.puts 'n'
-      $stdin = input
-      expect (show_inactive_org page).to eq('&a=1')
+      in25 = StringIO.open('Kaboom98')
+      in25.puts 'n'
+      $stdin = in25
+      expect(Searching.show_inactive_org).to(eq('&a=1'))
       $stdin = STDIN
     end
   end
